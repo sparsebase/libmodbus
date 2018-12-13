42c706315276a2a559429191fb1a9c2e179ab91a
Nan Zhang <zhangnan@baysenvision.com>
2018-12-13 10:05:18 +0800

    copying more header files to destination folder

--- src/win32/INSTALL/install.bat
+++ src/win32/INSTALL/install.bat
@@ -8,6 +8,8 @@ mkdir "C:\Program Files"\libmodbus\lib
 
 copy ..\modbus.h "C:\Program Files"\libmodbus\include\
 copy ..\modbus-version.h "C:\Program Files"\libmodbus\include\
+copy ..\modbus-tcp.h "C:\Program Files"\libmodbus\include\
+copy ..\modbus-rtu.h "C:\Program Files"\libmodbus\include\
 copy .\x64\Release\modbus.lib "C:\Program Files"\libmodbus\lib\
 copy .\x64\Release\modbus.dll "C:\Program Files"\libmodbus\bin\
 copy .\x64\Release\modbus.pdb "C:\Program Files"\libmodbus\bin\
--- src/win32/INSTALL/installd.bat
+++ src/win32/INSTALL/installd.bat
@@ -8,6 +8,8 @@ mkdir "C:\Program Files"\libmodbus\lib
 
 copy ..\modbus.h "C:\Program Files"\libmodbus\include\
 copy ..\modbus-version.h "C:\Program Files"\libmodbus\include\
+copy ..\modbus-tcp.h "C:\Program Files"\libmodbus\include\
+copy ..\modbus-rtu.h "C:\Program Files"\libmodbus\include\
 copy .\x64\Debug\modbusd.lib "C:\Program Files"\libmodbus\lib\
 copy .\x64\Debug\modbusd.dll "C:\Program Files"\libmodbus\bin\
 copy .\x64\Debug\modbusd.pdb "C:\Program Files"\libmodbus\bin\
